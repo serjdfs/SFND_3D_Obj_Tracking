3aedf08997b1a55bed0e8ca08072f95c38407554
serjdfs <serj_dfs@hotmail.com>
2020-01-31 14:05:50 +0100

    Added the Read Me file with the explanation of tasks and graphs.

--- src/FinalProject_Camera.cpp
+++ src/FinalProject_Camera.cpp
@@ -150,8 +150,8 @@ int main(int argc, const char *argv[])
 
         // extract 2D keypoints from current image
         vector<cv::KeyPoint> keypoints; // create empty feature list for current image
-        //string detectorType = "BRISK";
-        string detectorType = "SHITOMASI";
+        string detectorType = "BRISK";
+        //string detectorType = "SHITOMASI";
 
         if (detectorType.compare("SHITOMASI") == 0)
         {
